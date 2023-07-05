3c331fc79a65faa75aaf984351d71af75c931342
SnirNahum <snir09@gmail.com>
2023-07-05 11:25:48 +0300

    add routes for mail and notes

--- routes.js
+++ routes.js
@@ -1,20 +1,31 @@
-import HomePage from './views/HomePage.js'
-import AboutUs from './views/AboutUs.js'
+import HomePage from "./views/HomePage.js";
+import AboutUs from "./views/AboutUs.js";
 
-const { createRouter, createWebHashHistory } = VueRouter
+import MailIndex from './apps/mail/cmps/MailIndex.js'
+import NoteIndex from "./apps/keep/pages/NoteIndex.js";
+
+const { createRouter, createWebHashHistory } = VueRouter;
 
 const routerOptions = {
-	history: createWebHashHistory(),
-	routes: [
-		{
-			path: '/',
-			component: HomePage,
-		},
-		{
-			path: '/about',
-			component: AboutUs,
-		},
-	],
-}
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: "/",
+      component: HomePage,
+    },
+    {
+      path: "/about",
+      component: AboutUs,
+    },
+    {
+      path: "/mail",
+      component: MailIndex,
+    },
+    {
+      path: "/notes",
+      component: NoteIndex,
+    },
+  ],
+};
 
-export const router = createRouter(routerOptions)
+export const router = createRouter(routerOptions);
